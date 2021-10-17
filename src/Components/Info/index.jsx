ebe16b0a7f6b9f1e9ec4b027c6f30e5b24edfb07
Pai026 <abhirampai1999@gmail.com>
2021-10-17 09:23:37 +0530

    fixes font sizes

--- src/Components/Info/index.jsx
+++ src/Components/Info/index.jsx
@@ -4,15 +4,15 @@ const Info = () => {
       <div className="flex w-full space-x-6 text-2xl text-white">
         <div>
           <h3 className="text-gray-500">1996</h3>
-          <h1>Year</h1>
+          <h1 className="text-xs font-bold">Year</h1>
         </div>
         <div>
           <h3 className="text-gray-500">36</h3>
-          <h1>Number Of Cars</h1>
+          <h1 className="text-xs font-bold">Number Of Cars</h1>
         </div>
         <div>
           <h3 className="text-gray-500">20</h3>
-          <h1>Number Of Countries</h1>
+          <h1 className="text-xs font-bold">Number Of Countries</h1>
         </div>
       </div>
     </>
